13cbb9b856cf3a42aef596a4c91cff63f4dfaf8a
isaiale <108772859+isaiale@users.noreply.github.com>
2024-06-05 15:18:59 -0600

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hola Mundo'
+    return "Isai Alejandro Flores Grupo: A"
 
 if __name__ == '__main__':
     app.run(debug=True)
