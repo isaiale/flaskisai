9be52dee5bbe49c8738aeaa6bb2a4c32831f58c9
isaiale <isaialef20@gmail.com>
2024-06-06 09:40:47 -0600

    actualizacion

--- index.py
+++ index.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-<<<<<<< HEAD
     return render_template('index.html', name='Isai Alejandro Flores', group='A')
-=======
-    return "Isai Alejandro Flores Grupo: A"
->>>>>>> 13cbb9b856cf3a42aef596a4c91cff63f4dfaf8a
 
 if __name__ == '__main__':
     app.run(debug=True)
