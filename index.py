b4f990cbdf5cbd867e552a8998f79dc75daa3ed6
isaiale <isaialef20@gmail.com>
2024-06-06 09:15:26 -0600

    Add index.html

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hola Mundo'
+    return render_template('index.html', name='Isai Alejandro Flores', group='A')
 
 if __name__ == '__main__':
     app.run(debug=True)
